aa66d7fbe26ec0461b3d677a0a5141c2f50bf3ae
Ali Shamakhi <ali.shamakhi.97@gmail.com>
2017-10-15 17:13:39 +0330

    updated .gitignore

--- src/com/dblab/Main.java
+++ src/com/dblab/Main.java
@@ -1,8 +1,26 @@
 package com.dblab;
 
+import com.pengrad.telegrambot.Callback;
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.request.BaseRequest;
+import com.pengrad.telegrambot.response.BaseResponse;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private static String BOT_TOKEN = "339428453:AAHM90686AbdTuE-77Qa8cNaxkJU0arR7Ws";
 
+    public static void main(String[] args) {
+        TelegramBot bot = new TelegramBot(BOT_TOKEN);
+
+//        bot.execute(request, new Callback() {
+//            @Override
+//            public void onResponse(BaseRequest request, BaseResponse response) {
+//
+//            }
+//            @Override
+//            public void onFailure(BaseRequest request, IOException e) {
+//
+//            }
+//        });
     }
 }
