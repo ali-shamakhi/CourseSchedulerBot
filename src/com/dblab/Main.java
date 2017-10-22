1c1d4021c242452dc9f80fe24142eb1901520908
Ali Shamakhi <ali.shamakhi.97@gmail.com>
2017-10-22 15:58:12 +0330

    log messages to DB

--- src/com/dblab/Main.java
+++ src/com/dblab/Main.java
@@ -2,6 +2,7 @@ package com.dblab;
 
 import com.pengrad.telegrambot.Callback;
 import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.BaseRequest;
@@ -10,6 +11,9 @@ import com.pengrad.telegrambot.response.BaseResponse;
 import com.pengrad.telegrambot.response.GetUpdatesResponse;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 public class Main {
@@ -19,9 +23,39 @@ public class Main {
     public static void main(String[] args) {
         TelegramBot bot = new TelegramBot(BOT_TOKEN);
 
-        GetUpdates getUpdates = new GetUpdates().limit(100).offset(0).timeout(0);
+        Connection connection = null;
 
-        final GetUpdatesResponse updatesResponse = bot.execute(getUpdates);
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/dblab_log", "bot_user", "botbotpass");
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        final Connection conn = connection;
+
+        bot.setUpdatesListener(new UpdatesListener() {
+//            @Override
+            public int process(List<Update> updates) {
+
+                for (Update update : updates) {
+
+//                    System.out.println(update.message().from());
+
+                    try {
+                        conn.createStatement().execute("INSERT INTO log(text, user_id) VALUES(\"" + update.message().text() + "\", " + update.message().from().id() + ")");
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                return UpdatesListener.CONFIRMED_UPDATES_ALL;
+            }
+        });
 
 //        // sync
 //        List<Update> updates = updatesResponse.updates();
@@ -31,20 +65,20 @@ public class Main {
 
 
 // async
-        bot.execute(getUpdates, new Callback<GetUpdates, GetUpdatesResponse>() {
-//            @Override
-            public void onResponse(GetUpdates request, GetUpdatesResponse response) {
-                List<Update> updates = updatesResponse.updates();
-                for (Update update : updates) {
-                    System.out.println(update.message().toString());
-                }
-            }
-
-//            @Override
-            public void onFailure(GetUpdates request, IOException e) {
-
-            }
-        });
+//        bot.execute(getUpdates, new Callback<GetUpdates, GetUpdatesResponse>() {
+////            @Override
+//            public void onResponse(GetUpdates request, GetUpdatesResponse response) {
+//                List<Update> updates = updatesResponse.updates();
+//                for (Update update : updates) {
+//                    System.out.println(update.message().toString());
+//                }
+//            }
+//
+////            @Override
+//            public void onFailure(GetUpdates request, IOException e) {
+//
+//            }
+//        });
 
     }
 }
