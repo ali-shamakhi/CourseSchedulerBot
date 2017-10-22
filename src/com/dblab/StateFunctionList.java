7848a199205e7b58e5e9f5acc4724e49987289c0
Ali Shamakhi <ali.shamakhi.97@gmail.com>
2017-10-22 17:44:26 +0330

    implemented StateFunctionList.validate()

---
+++ src/com/dblab/StateFunctionList.java
@@ -0,0 +1,34 @@
+package com.dblab;
+
+import com.pengrad.telegrambot.Callback;
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.request.ParseMode;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
+
+import java.io.IOException;
+
+public class StateFunctionList {
+    static void validate(Message message) {
+
+        SendMessage request;
+
+            request = new SendMessage(message.chat().id(), "1. Add course\n2. List courses\n3. Delete course")
+                    .parseMode(ParseMode.Markdown)
+                    .disableWebPagePreview(true)
+                    .disableNotification(true);
+
+
+        Main.bot.execute(request, new Callback<SendMessage, SendResponse>() {
+            public void onResponse(SendMessage sendMessage, SendResponse sendResponse) {
+                System.out.println(sendMessage.toString());
+                System.out.println(sendResponse.toString());
+            }
+
+            public void onFailure(SendMessage sendMessage, IOException e) {
+
+            }
+        });
+
+    }
+}
