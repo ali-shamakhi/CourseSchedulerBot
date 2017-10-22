70aeb5e7217c17e1264a96614d4d844ae7da5829
sanei <saneimousavi93@gmail.com>
2017-10-22 16:56:15 +0330

    init states

---
+++ src/com/dblab/StateHandler.java
@@ -0,0 +1,39 @@
+package com.dblab;
+
+import com.pengrad.telegrambot.model.Message;
+import com.sun.glass.ui.EventLoop;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by sanei on 10/22/2017.
+ */
+public class StateHandler {
+
+//    public static final String S_NEW_USER = "NEW_USER";
+    public static final String S_MAIN_SCREEN = "MAIN_SCREEN";
+    public static final String S_FUNCTION_LIST = "FUNCTION_LIST";
+
+    static void handleState(Message incomingMessage, final Connection connection) throws SQLException{
+        ResultSet result = connection.createStatement().executeQuery("SELET state from user_state_tbl WHERE user_id = " + incomingMessage.from().id());
+        String state = null;
+        while (result.next()) {
+            state = result.getString("state");
+        }
+        if (state == null) {
+            StateNewUser.validate(incomingMessage.from().id());
+        } else {
+            if (state.equals(S_MAIN_SCREEN)) {
+                StateMainScreen.validate(incomingMessage.from().id());
+            }
+            else if (state.equals(S_FUNCTION_LIST)) {
+                StateFunctionList.validate(incomingMessage.from().id());
+            }
+            else {
+                System.err.println("Unknown state " + state + " for user_id " + incomingMessage.from().id());
+            }
+        }
+    }
+}
