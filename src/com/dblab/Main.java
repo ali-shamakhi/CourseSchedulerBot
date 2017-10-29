16f6f9ed23572e5cc04101cc7ccff7a70f78d6a9
sanei <saneimousavi93@gmail.com>
2017-10-29 15:49:19 +0330

    minor changes

--- src/com/dblab/Main.java
+++ src/com/dblab/Main.java
@@ -1,16 +1,9 @@
 package com.dblab;
 
-import com.pengrad.telegrambot.Callback;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
-import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
-import com.pengrad.telegrambot.request.BaseRequest;
-import com.pengrad.telegrambot.request.GetUpdates;
-import com.pengrad.telegrambot.response.BaseResponse;
-import com.pengrad.telegrambot.response.GetUpdatesResponse;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -23,13 +16,14 @@ public class Main {
     static TelegramBot bot = null;
 
     public static void main(String[] args) {
+
         bot = new TelegramBot(BOT_TOKEN);
 
         Connection connection = null;
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost/dblab_log", "bot_user", "botbotpass");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/dblab_log", LocalConfigs.USERNAME, LocalConfigs.PASSWORD);
         } catch (SQLException se) {
             se.printStackTrace();
         }
@@ -44,44 +38,16 @@ public class Main {
             public int process(List<Update> updates) {
 
                 for (Update update : updates) {
-
-//                    System.out.println(update.message().from());
-
                     try {
                         conn.createStatement().execute("INSERT INTO log(text, user_id) VALUES(\"" + update.message().text() + "\", " + update.message().from().id() + ")");
                         StateHandler.handleState(update.message(), conn);
                     } catch (SQLException e) {
                         e.printStackTrace();
                     }
-
                 }
 
                 return UpdatesListener.CONFIRMED_UPDATES_ALL;
             }
         });
-
-//        // sync
-//        List<Update> updates = updatesResponse.updates();
-//        for (Update update : updates) {
-//            System.out.println(Message message = update.message());
-//        }
-
-
-// async
-//        bot.execute(getUpdates, new Callback<GetUpdates, GetUpdatesResponse>() {
-////            @Override
-//            public void onResponse(GetUpdates request, GetUpdatesResponse response) {
-//                List<Update> updates = updatesResponse.updates();
-//                for (Update update : updates) {
-//                    System.out.println(update.message().toString());
-//                }
-//            }
-//
-////            @Override
-//            public void onFailure(GetUpdates request, IOException e) {
-//
-//            }
-//        });
-
     }
 }
