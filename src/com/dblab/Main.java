8101e6f180a90c1ff0297308a1c0378c2c434825
sanei <saneimousavi93@gmail.com>
2017-10-29 16:43:16 +0330

    fix pre-commit db

--- src/com/dblab/Main.java
+++ src/com/dblab/Main.java
@@ -22,7 +22,6 @@ public class Main {
         Connection connection = null;
 
         try {
-
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection("jdbc:mysql://localhost/dblab_log", LocalConfigs.USERNAME, LocalConfigs.PASSWORD);
         } catch (SQLException se) {
