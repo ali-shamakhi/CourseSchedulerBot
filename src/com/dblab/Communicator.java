02bcf36e13e03ba71fdb8d133b94175371931042
Ali Shamakhi <ali.shamakhi.97@gmail.com>
2017-11-04 20:44:42 +0330

    added Communicator.paramString(string, params)

--- src/com/dblab/Communicator.java
+++ src/com/dblab/Communicator.java
@@ -7,6 +7,7 @@ import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 
 import java.io.IOException;
+import java.util.IllegalFormatException;
 
 public class Communicator {
 
@@ -26,6 +27,18 @@ public class Communicator {
                 e.printStackTrace();
             }
         });
+    }
+
+    /**
+     * Replaces every "%s" in <code>string</code> with corresponding <code>params</code>
+     * @param string initial string
+     * @param params values defined dynamically in runtime
+     * @return the result string
+     * @exception IllegalFormatException thrown if the length if <code>params</code> does not match the number of "%s" in <code>string</code>
+     */
+    public static String paramString(String string, String ... params) throws IllegalFormatException {
+        if ((params == null || params.length == 0) && !string.contains("%s")) return string;
+        return String.format(string, params);
     }
 
 }
