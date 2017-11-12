b2f1f86552fb648e85dc61092a77322892b57f2e
sanei <saneimousavi93@gmail.com>
2017-11-12 14:59:00 +0330

    comment fixed!

--- src/com/dblab/state/StateRegistrationGetEntranceYear.java
+++ src/com/dblab/state/StateRegistrationGetEntranceYear.java
@@ -16,7 +16,7 @@ public class StateRegistrationGetEntranceYear {
     public static void validate(Message message) throws SQLException {
         if (1301 < Integer.parseInt(message.text()) && Integer.parseInt(message.text()) < 1397) {
 
-            // Will change to DBHelper.setStudentSubstate(message.from().id(), NULL);
+            // DBHelper.setStudentSubstate(message.from().id(), NULL);
             DBHelper.setStudentState(message.from().id(), StateMainScreen.VALUE);
             Communicator.sendMessage(Main.bot, message.chat().id(), "The registration was successful!\nPress /menu to see all functions.");
         }
