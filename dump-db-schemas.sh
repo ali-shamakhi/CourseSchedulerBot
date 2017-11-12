fe307f6963e8b856382e48f12b291a5c36c1c5da
Ali Shamakhi <ali.shamakhi.97@gmail.com>
2017-11-12 15:24:49 +0330

    added stored procedure SET_STUDENT_MAJOR_BY_FIELDS

--- dump-db-schemas.sh
+++ dump-db-schemas.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 source ./db-constants.sh
-mysqldump --login-path=telbot --no-data --skip-dump-date $DBNAME > SQLVersionControl/schema.sql
+mysqldump --login-path=telbot --no-data --routines --skip-dump-date $DBNAME > SQLVersionControl/schema.sql
 git add SQLVersionControl/schema.sql
 exit 0
