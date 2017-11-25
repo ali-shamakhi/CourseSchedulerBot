16fc9877d136727a61c0400d38782e91c8e6123e
Ali Shamakhi <ali.shamakhi.97@gmail.com>
2017-11-25 21:28:14 +0330

    handled NumberFormatException for bad entrance year

--- src/com/dblab/state/StateRegistrationGetEntranceYear.java
+++ src/com/dblab/state/StateRegistrationGetEntranceYear.java
@@ -16,16 +16,19 @@ public class StateRegistrationGetEntranceYear {
     public static final String VALUE = "REGISTRATION_GET_ENTRANCE_YEAR";
 
     public static void validate(Message message, UserMap<MajorModel> userMajorMap) throws SQLException {
-        if (1301 < Integer.parseInt(message.text()) && Integer.parseInt(message.text()) < 1397) {
-            userMajorMap.get(message.from().id()).entranceYear = Integer.parseInt(message.text());
-            DBHelper.setStudentMajorByFields(message.from().id(), userMajorMap.get(message.from().id()));
-            // Check it:
-            DBHelper.setStudentSubstate(message.from().id(), "");
-            DBHelper.setStudentState(message.from().id(), StateMainScreen.VALUE);
-            Communicator.sendMessage(Main.bot, message.chat().id(), "The registration was successful!\nPress /menu to see all functions.");
-        }
-        else {
-            Communicator.sendMessage(Main.bot, message.chat().id(), "Entrance year should be between 1301 to 1396");
+        try {
+            if (1301 < Integer.parseInt(message.text()) && Integer.parseInt(message.text()) < 1397) {
+                userMajorMap.get(message.from().id()).entranceYear = Integer.parseInt(message.text());
+                DBHelper.setStudentMajorByFields(message.from().id(), userMajorMap.get(message.from().id()));
+                // Check it:
+                DBHelper.setStudentSubstate(message.from().id(), null);
+                DBHelper.setStudentState(message.from().id(), StateMainScreen.VALUE);
+                Communicator.sendMessage(Main.bot, message.chat().id(), "The registration was successful!\nPress /menu to see all functions.");
+            } else {
+                Communicator.sendMessage(Main.bot, message.chat().id(), "Entrance year should be between 1301 to 1396");
+            }
+        } catch (NumberFormatException e) {
+            Communicator.sendMessage(Main.bot, message.chat().id(), "Please enter a valid number between 1301 to 1396");
         }
     }
 }
